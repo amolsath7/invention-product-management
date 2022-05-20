1214e98234d518998171d0fd916501ff2eeed6f8
Amol Sathewad <amolsath@amdocs.com>
2022-05-20 13:42:40 +0530

    updated changes

--- src/main/java/com/amdocs/ensemble/util/Constant.java
+++ src/main/java/com/amdocs/ensemble/util/Constant.java
@@ -1,10 +1,13 @@
 package com.amdocs.ensemble.util;
 
+import lombok.Data;
+
+@Data
 public class Constant {
-    public static String DATA_NOT_FOUND = "Data not found";
-    public static String NOT_STARTED = "Not started";
-    public static String PLANNING = "Planning";
-    public static String IN_PROGRESS = "In-Progress";
-    public static String COMPLETED = "Completed";
+    public static final String DATA_NOT_FOUND = "Data not found";
+    public static final String NOT_STARTED = "Not started";
+    public static final String PLANNING = "Planning";
+    public static final String IN_PROGRESS = "In-Progress";
+    public static final String COMPLETED = "Completed";
 
 }
